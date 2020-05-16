3250ac48c7abfff7bb277812dc5729defdc983ea
tuananhzippy <kieutuananh1995@gmail.com>
2020-05-16 09:21:32 +0700

    fix: protocol enqueue after fatal error

--- services/db-connection.js
+++ services/db-connection.js
@@ -1,24 +1,26 @@
 module.exports = DbConnection;
 var mysql = require('mysql');
-var connection = null;
+var pool = null;
 
 function DbConnection($config, $event, $logger) {
     this.connect = function(config) {
-        connection = mysql.createConnection(config);
-        connection.connect();
+        config.connectionLimit = 200;
+        pool = mysql.createPool(config);
     };
 
     this.query = async function (queryString) {
         return new Promise(function (resolve, reject) {
-            connection.query(queryString,
-                function (error, results, fields) {
-                    if (error) {
-                        reject(error);
-                    } else {
-                        resolve(results);
+            pool.getConnection(function(err, connection) {
+                connection.query(queryString,
+                    function (error, results, fields) {
+                        if (error) {
+                            reject(error);
+                        } else {
+                            resolve(results);
+                        }
                     }
-                }
-            );
+                );
+            });
         });
     };
 }
